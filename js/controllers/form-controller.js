2196dd83a38cb840951cf6e729db354dc45d2ab7
Samuel Quaresma <squoliver@gmail.com>
2025-07-06 20:56:34 -0300

    Validação do address antes de salvar

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -65,7 +65,18 @@ async function handleInputCepChange(event) {
 
 async function handleBtnSaveClick(event) {
     event.preventDefault();
-    listController.addCard(state.address);
+
+    const errors = addressService.getErrors(state.address);
+
+    const keys = Object.keys(errors);
+    if(keys.length > 0) {
+        keys.forEach(key => {
+            setFormError(key, errors[key]);
+        });
+    } else {
+        listController.addCard(state.address);
+        clearForm();
+    }
 }
 
 function handleBtnClearClick(event) {
@@ -81,6 +92,8 @@ function clearForm() {
 
     setFormError("cep", "");
     setFormError("number", "");
+
+    state.address = new Address();
 
     state.inputCep.focus();
 }
